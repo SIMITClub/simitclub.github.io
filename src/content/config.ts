0917c03d2a907afb874aba6cddc9697d4472ef5c
mkamsani <mkamsani@posteo.org>
2023-06-21 01:12:23 +0800

    Add site schema (incomplete)

---
+++ src/content/config.ts
@@ -0,0 +1,47 @@
+import { defineCollection, reference, z } from 'astro:content';
+
+const memberCollection = defineCollection({
+    type: 'data',
+    schema: z
+        .object({
+
+            name: z.object({
+                first: z.string(),
+                last: z.string().optional(),
+            }),
+
+        }).strict()
+});
+
+const categoryCollection = defineCollection({
+    type: 'data',
+    schema: z
+        .object({
+
+            title: z.string(),
+            description: z.string(),
+
+        }).strict()
+});
+
+const postCollection = defineCollection({
+    type: "content",
+    schema: z
+        .object({
+
+            authors: z
+                .array(reference('members'))
+                .default(['_default']),
+            categories: z
+                .array(reference('category'))
+                .default([]),
+            title: z.string(),
+
+        }).strict()
+});
+
+export const collections = {
+    'members': memberCollection,
+    'category': categoryCollection,
+    'posts': postCollection,
+};
