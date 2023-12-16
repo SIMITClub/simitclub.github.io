80422c6c71f1b8cc0c26f2438deadfe259b4c021
itsJae <dnwodnwo0924@naver.com>
2023-12-17 01:45:31 +0900

    Added the dark theme on the homepage

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 import { SITE_URL } from './src/consts';
 
 export default defineConfig({
-	site: SITE_URL,
-	build: {
-		inlineStylesheets: 'always',
-	},
+    site: SITE_URL,
+
+    build: {
+        inlineStylesheets: "always",
+    },
+    image: {
+        service: sharpImageService(),
+    }
 });
--- src/content/members/!default.md
+++ src/content/members/!default.md
@@ -1,9 +1,9 @@
 ---
 name:
-  first: 'SIM'
-  last: 'IT Club'
-bio: 'The official author page of the club.'
+  first: "SIM"
+  last: "IT Club"
+bio: "The official author page of the club."
 img:
-  src: '@images/logo-dark.png'
-  alt: 'SIM IT Club'
+  src: "@images/logo-dark.png"
+  alt: "SIM IT Club"
 ---
