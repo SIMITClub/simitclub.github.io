de697391f9cd9e182501b117a44c0b0f385ffb00
mkamsani <mkamsani@posteo.org>
2023-12-31 13:53:09 +0800

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,28 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		icon({
+			svgoOptions: {
+				multipass: false, plugins: [{
+					name: "preset-default",
+					params: { overrides: {
+						cleanupIds: {remove: false, minify: true, force: false,},
+						inlineStyles: {
+							onlyMatchedOnce: false,
+							removeMatchedSelectors: false,
+							useMqs: ["screen", "print", "prefers-color-scheme"],
+						},
+					}},
+			}]}
+		})
+	]
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'SIM IT Club';
+export const SITE_DESCRIPTION = 'Official website of SIM IT Club.';
+export const SITE_URL = 'https://simitclub.github.io/';
