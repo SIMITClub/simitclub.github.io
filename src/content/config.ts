19e9a58e14a706be2bff3c0e3989b823b8f2514c
mkamsani <mkamsani@posteo.org>
2024-02-13 14:06:39 +0800

    Fix images not showing in production (npm run build)

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,13 +3,16 @@ import { defineCollection, z } from 'astro:content';
 const blog = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
-	schema: z.object({
+	schema: ({ image }) => z.object({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		heroImage: image().refine((img) => img.width >= 1280, {
+			message: "Cover image must be at least 1280 pixels wide!",
+		}).optional(),
+		draft: z.boolean().optional()
 	}),
 });
 
