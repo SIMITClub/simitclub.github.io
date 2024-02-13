f8251da3564ab0ee39b8c52ca68709907fc9cf4e
mkamsani <mkamsani@posteo.org>
2024-02-13 14:26:28 +0800

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import icon from "astro-icon";
+import { SITE_URL } from "src/consts";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: SITE_URL,
 	integrations: [
 		mdx(),
 		sitemap(),
